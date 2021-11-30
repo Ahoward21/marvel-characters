81d84900778702f886d5c72166662ce315fbecbb
GrumpyMonk <anhoward1988@gmail.com>
2021-11-29 23:19:08 -0800

    text color

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var charactersEl = document.getElementById('characters');
+var publicKey = "f5e0d5c0b51df5fdcd857f97593050b6";
 
 fetch('https://gateway.marvel.com:443/v1/public/characters?apikey=f5e0d5c0b51df5fdcd857f97593050b6').then(function(response) {
   return response.json();
@@ -8,7 +9,8 @@ fetch('https://gateway.marvel.com:443/v1/public/characters?apikey=f5e0d5c0b51df5
     var character = data.data.results[i];
     console.log('character is', character);
     var newListItem = document.createElement('li');
-    newListItem.innerText = character.id;
+    newListItem.innerText = character.name
+    ;
     charactersEl.appendChild(newListItem);
   }
 });
