dfda10f990fb9b633b3ed5e14009dcfcf59d608c
GrumpyMonk <anhoward1988@gmail.com>
2021-11-30 22:21:24 -0800

    test

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ fetch('https://gateway.marvel.com:443/v1/public/characters?apikey=f5e0d5c0b51df5
   }
 });
 console.log("is this working")
-fetch('https://comicvine.gamespot.com/api/issues/?api_key=7a85ae236d0baed1243a70bd84fdd22db445b36e')
+fetch('https://comicvine.gamespot.com/api/issues/?api_key=7a85ae236d0baed1243a70bd84fdd22db445b36e&format=json')
 .then(function(response) {
     console.log("im here")
     return response.json();
